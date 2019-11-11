deaf3ee237939fa3d6d89ec8f9404caae95deed7
Eric Fredine <eric.fredine@beanworks.com>
2019-11-10 17:15:20 -0800

    Fixup - delete unused code.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-// $startButton.onclick = () => {
-//   // Get active tab
 
 function Button() {
   function handleClick() {
--- src/popup.js
+++ src/popup.js
@@ -13,25 +13,6 @@ function ExtensionApp() {
 console.log('Executing popup.js');
 
 window.onload = () => {
-  //   console.log('Window loaded -- from popup.js');
-  //   const $startButton = document.querySelector('.start');
-
-  //   $startButton.onclick = () => {
-  //     // Get active tab
-  //     chrome.tabs.query(
-  //       {
-  //         active: true,
-  //         currentWindow: true,
-  //       },
-  //       tabs => {
-  //         // Send message to script file
-  //         chrome.tabs.sendMessage(tabs[0].id, { injectApp: true }, response =>
-  //           window.close()
-  //         );
-  //       }
-  //     );
-  //   };
-
   const $container = document.querySelector('#container');
   ReactDOM.render(<ExtensionApp />, $container);
 };
