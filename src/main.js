a40a2c65047b052de1aec4fcc3f1508c5951afed
Eric Fredine <eric.fredine@beanworks.com>
2019-11-17 08:06:26 -0800

    Grabs document text using innerText rather than a treewalker.

--- src/main.js
+++ src/main.js
@@ -7,31 +7,13 @@ chrome.runtime.onMessage.addListener((request, sender, response) => {
   }
 });
 
-function bookmarkPage(response) {
-  const treeWalker = document.createTreeWalker(
-    document.body,
-    NodeFilter.SHOW_TEXT,
-    function(node) {
-      return node.textContent
-        ? NodeFilter.FILTER_ACCEPT
-        : NodeFilter.FILTER_SKIP;
-    },
-    false
-  );
-
-  let content = '';
-  while (treeWalker.nextNode()) {
-    if (treeWalker.currentNode && treeWalker.currentNode.textContent) {
-      content += treeWalker.currentNode.textContent;
-    }
-  }
-
+const bookmarkPage = response => {
   insertPage(
     {
-      content,
+      content: document.documentElement.innerText,
       title: document.title,
       url: window.location.href,
     },
     response
   );
-}
+};
